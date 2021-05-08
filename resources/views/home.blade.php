ded9bcda5685ae0688dfc4bbc05dbdd8bd9c6018
Tiago <tfranca2@gmail.com>
2021-05-08 14:32:34 -0300

    deploy

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Dashboard.</div>
                 <div class="card-body"><br></div>
             </div>
 
