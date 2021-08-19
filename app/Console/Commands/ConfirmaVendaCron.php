f8c16c8497ea16b375d79764f5332eb8c9f9c0c0
Tiago <tfranca2@gmail.com>
2021-08-18 22:02:33 -0300

    integração API STER

--- app/Console/Commands/ConfirmaVendaCron.php
+++ app/Console/Commands/ConfirmaVendaCron.php
@@ -43,13 +43,49 @@ class ConfirmaVendaCron extends Command
     public function handle()
     {
 
-        $vendasNaoConfirmadas = Venda::where('confirmada',0)->whereRaw(' NOW() > date_add( created_at, interval 30 minute ) ')->get();
+        $vendasNaoConfirmadas = Venda::where('confirmada',0)->whereNull('protocolo')->whereRaw(' NOW() > date_add( created_at, interval 30 minute ) ')->get();
         foreach( $vendasNaoConfirmadas as $venda ){
-
             DB::beginTransaction();
             try {
                 VendaMatriz::where('venda_id',$venda->id)->delete();
                 Venda::where('id',$venda->id)->delete();
+                DB::commit();
+            } catch( Exception $e ){
+                DB::rollBack();
+            }
+        }
+
+        $vendasNaoConfirmadasCorreios = Venda::where('confirmada',0)->whereNotNull('protocolo')->whereRaw(' NOW() > date_add( created_at, interval 1 day ) ')->get();
+        foreach( $vendasNaoConfirmadasCorreios as $venda ){
+
+            DB::beginTransaction();
+            try {
+
+                // consultar protocolo
+                $data = [
+                    'numeroProtocolo' => $venda->protocolo,
+                ];
+
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_POST, TRUE);
+                curl_setopt($ch, CURLOPT_URL, 'https://apps.correios.com.br/ster/api/confirmarAtendimento');
+                curl_setopt($ch, CURLOPT_HEADER, FALSE);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+                curl_setopt($ch, CURLOPT_HTTPHEADER,[ "Content-Type: application/json" ]);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+                $response = curl_exec($ch);
+                $status_code = curl_getinfo($ch)['http_code'];
+                curl_close($ch);
+
+                $response = json_decode($response);
+                if ($status_code >= 300 || $status_code < 200 || !$response || $response->codigoConfirmacao == '99') {
+                    VendaMatriz::where('venda_id',$venda->id)->delete();
+                    Venda::where('id',$venda->id)->delete();
+                } else {
+                    $venda->confirmada = true;
+                    $venda->save();
+                }
+
                 DB::commit();
             } catch( Exception $e ){
                 DB::rollBack();
