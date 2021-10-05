17e65655331e8fa7646b525b2d2d57e40c79d547
Tiago <tfranca2@gmail.com>
2021-10-05 16:05:13 -0300

    mudança tabela matrizes

--- app/Matriz.php
+++ app/Matriz.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Matriz extends Model
 {
-    protected $table = 'matrizes';
+    // protected $table = 'matrizes';
+    protected $table = 'matriz_50_15';
 	protected $fillable = [ 'bilhete', 'combinacoes' ];
 }
