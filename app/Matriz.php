d1add8143cd92e27de516e9d80e80aa6cbc536a4
Tiago <tfranca2@gmail.com>
2021-10-25 09:29:37 -0300

    bugfix matriz table

--- app/Matriz.php
+++ app/Matriz.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Matriz extends Model
 {
-    protected $table = env('MATRIZ', 'matrizes');
+    protected $table = 'matrizes';
 	protected $fillable = [ 'bilhete', 'combinacoes' ];
+
+	function __construct(){
+	    $this->table = env('MATRIZ', 'matrizes');
+	}
 }
